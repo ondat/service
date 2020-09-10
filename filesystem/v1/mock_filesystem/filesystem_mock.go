5cc804b3599e25c52418d6f7815681e5babaad43
André Lucas <andre@ae-35.com>
2020-09-10 17:32:40 +0100

    Generated go.

--- filesystem/v1/mock_filesystem/filesystem_mock.go
+++ filesystem/v1/mock_filesystem/filesystem_mock.go
@@ -217,14 +217,14 @@ func (mr *MockFsClientMockRecorder) ConfigUpdateUint32(arg0, arg1 interface{}, a
 }
 
 // PresentationCreate mocks base method.
-func (m *MockFsClient) PresentationCreate(arg0 context.Context, arg1 *v10.FsPresentation, arg2 ...grpc.CallOption) (*v1.RpcResult, error) {
+func (m *MockFsClient) PresentationCreate(arg0 context.Context, arg1 *v10.FsPresentation, arg2 ...grpc.CallOption) (*v10.FsPresentationCreateResponse, error) {
 	m.ctrl.T.Helper()
 	varargs := []interface{}{arg0, arg1}
 	for _, a := range arg2 {
 		varargs = append(varargs, a)
 	}
 	ret := m.ctrl.Call(m, "PresentationCreate", varargs...)
-	ret0, _ := ret[0].(*v1.RpcResult)
+	ret0, _ := ret[0].(*v10.FsPresentationCreateResponse)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
