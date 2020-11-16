04e95238678fcc4e97602e4ea18110f2fcfe3702
Alex Reid <alex.reid@storageos.com>
2020-11-16 16:36:04 +0000

    Re-instate the previously removed StatsList RPC()

--- directfs/v1/mock_directfs/dfs_responder_mock.go
+++ directfs/v1/mock_directfs/dfs_responder_mock.go
@@ -157,6 +157,26 @@ func (mr *MockDfsResponderClientMockRecorder) ConfigUpdateString(arg0, arg1 inte
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ConfigUpdateString", reflect.TypeOf((*MockDfsResponderClient)(nil).ConfigUpdateString), varargs...)
 }
 
+// StatsList mocks base method.
+func (m *MockDfsResponderClient) StatsList(arg0 context.Context, arg1 *v1.StatsListQuery, arg2 ...grpc.CallOption) (*v1.StatsListResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{arg0, arg1}
+	for _, a := range arg2 {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "StatsList", varargs...)
+	ret0, _ := ret[0].(*v1.StatsListResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// StatsList indicates an expected call of StatsList.
+func (mr *MockDfsResponderClientMockRecorder) StatsList(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{arg0, arg1}, arg2...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "StatsList", reflect.TypeOf((*MockDfsResponderClient)(nil).StatsList), varargs...)
+}
+
 // Status mocks base method.
 func (m *MockDfsResponderClient) Status(arg0 context.Context, arg1 *v10.DfsResponderStatusRequest, arg2 ...grpc.CallOption) (*v10.DfsResponderStatus, error) {
 	m.ctrl.T.Helper()
